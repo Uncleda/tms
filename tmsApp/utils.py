365202c1e0901e864237a118146efe6a224af815
Huang Dongjie <dongjiehuang@gmail.com>
2016-01-12 15:15:11 +0800

    devide monitor function into three parts - cpu, memory, disk.

--- tmsApp/utils.py
+++ tmsApp/utils.py
@@ -61,7 +61,12 @@ def refresh_term(modeladmin, request, queryset):
     for instance in queryset:
         host_list.append(instance.ip)
 
-    term_info = execute(get_term_info, hosts = host_list)
+    try:
+        term_info = execute(get_term_info, hosts = host_list)
+        showUpdatedResult(modeladmin, request, len(output))
+
+    except:
+        showUpdatedResult(modeladmin, request)
 
     save_changes(term_info)
 
@@ -71,8 +76,13 @@ def poweron_selected_terms(modeladmin, request, queryset):
     '''
     Power on the remote terminals with the wakeonlan tool
     '''
-    for instance in queryset:
-        os.system('/usr/bin/wakeonlan ' + instance.mac)
+    try:
+        for instance in queryset:
+            os.system('wakeonlan ' + instance.mac)
+        showUpdatedResult(modeladmin, request, len(queryset))
+
+    except:
+        showUpdatedResult(modeladmin, request)
 
 poweron_selected_terms.short_description = "Poweron selected terminals"
 
@@ -84,7 +94,12 @@ def shutdown_selected_terms(modeladmin, request, queryset):
     for instance in queryset:
         host_list.append(instance.ip)
 
-    result = execute(shutdown_term, hosts = host_list)
+    try:
+        result = execute(shutdown_term, hosts = host_list)
+        showUpdatedResult(modeladmin, request, len(host_list))
+
+    except:
+        showUpdatedResult(modeladmin, request)
 
     print result
 
@@ -98,35 +113,89 @@ def reboot_selected_terms(modeladmin, request, queryset):
     for instance in queryset:
         host_list.append(instance.ip)
 
-    result = execute(reboot_term, hosts = host_list)
+    try:
+        result = execute(reboot_term, hosts = host_list)
+        showUpdatedResult(modeladmin, request, len(host_list))
+
+    except:
+        showUpdatedResult(modeladmin, request)
 
     print result
 
 reboot_selected_terms.short_description = "Reboot selected terminals"
 
-def monitor_term(modeladmin, request, queryset):
+def monitor_term(modeladmin, request, queryset, item = 'all'):
     '''
     Redirect to monitor information display page
     '''
     host_list = []
+    tags = {}
     for instance in queryset:
         host_list.append(instance.ip)
+        tags[instance.ip] = instance.tag
 
-    monitor_info = execute(get_monitor_info, hosts = host_list)
+    try:
+        monitor_info = execute(get_monitor_info, hosts = host_list)
+
+    except:
+        showUpdatedResult(modeladmin, request)
+
+    for ip, result in monitor_info.items():
+        monitor_info[ip] = sorted(result.iteritems(), key=lambda asd:asd[0])
+        monitor_info[ip][0] = ('1_tag', tags[ip])
+
+    if item == 'all':
+        template = "monitor_info.html"
+    if item == 'cpu':
+        template = "monitor_cpu.html"
+        for ip, result in monitor_info.items():
+            del monitor_info[ip][3:9]
+    if item == 'mem':
+        template = "monitor_mem.html"
+        for ip, result in monitor_info.items():
+            del monitor_info[ip][1:3]
+            del monitor_info[ip][4:]
+    if item == 'disk':
+        template = "monitor_disk.html"
+        for ip, result in monitor_info.items():
+            del monitor_info[ip][1:6]
 
     print monitor_info
 
     context = dict(
         modeladmin.admin_site.each_context(request),
-        title=_("Monitor Information"),
+        title="Monitor Information",
         monitor_info=dict(monitor_info).items(),
     )
 
     request.current_app = modeladmin.admin_site.name
 
     # Display the monitor page
-    return TemplateResponse(request, "monitor_info.html", context)
+    return TemplateResponse(request, template, context)
     #return render(request, "admin/monitor_info.html", {'form':form})
 
 monitor_term.short_description = "Display monitor page"
 
+def monitor_cpu(modeladmin, request, queryset):
+    '''
+    Redirect to monitor cpu page
+    '''
+    return monitor_term(modeladmin, request, queryset, item = 'cpu')
+
+monitor_cpu.short_description = "Display cpu usage"
+
+def monitor_mem(modeladmin, request, queryset):
+    '''
+    Redirect to monitor memory page
+    '''
+    return monitor_term(modeladmin, request, queryset, item = 'mem')
+
+monitor_mem.short_description = "Display memory usage"
+
+def monitor_disk(modeladmin, request, queryset):
+    '''
+    Redirect to monitor disk page
+    '''
+    return monitor_term(modeladmin, request, queryset, item = 'disk')
+
+monitor_disk.short_description = "Display disk usage"
