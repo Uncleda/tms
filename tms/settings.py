c4a17c28fae0686e597566643883a877341e27fe
JunpengChen <junpeng@nfs-qd.com>
2016-01-13 13:01:01 +0800

    modify database setting

--- tms/settings.py
+++ tms/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
 	'NAME': 'tmsDB',
-	'USER': 'root',
-	'PASSWORD': 'hello.1234',
+	'USER': 'tmsAdmin',
+	'PASSWORD': 'bhu890-=',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
